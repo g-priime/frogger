f5ae8084e559e423f0eb31e10d7845348d1eaf23
g-priime <57049481+g-priime@users.noreply.github.com>
2022-02-12 15:20:48 -0700

    Update app.js

--- app.js
+++ app.js
@@ -126,7 +126,11 @@ function moveCarRight(carRight) {
 }
 
 function lose() {
-  if (squares[currentIndex].classList.contains("c1")) {
+  if (
+    squares[currentIndex].classList.contains("c1") ||
+    squares[currentIndex].classList.contains("l4") ||
+    squares[currentIndex].classList.contains("l5")
+  ) {
     resultDisplay.textContent = "You lose";
     clearInterval(timerId);
     squares[currentIndex].classList.remove("frog");
