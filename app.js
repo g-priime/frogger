2287d6269d3988a27b2868227679ec96e9423a46
g-priime <57049481+g-priime@users.noreply.github.com>
2022-02-12 13:55:01 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const timeLeftDisplay = document.querySelector('#time-left')
+const resultDisplay = document.querySelector('#result')
+const StartPauseButton = document.querySelector('#start-pause-button')
+const squares = document.querySelectorAll('.grid div')
+
+function moveFrog() {
+    console.log('moved')
+}
+document.addEventListener('keyup', moveFrog)
