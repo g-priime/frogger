182ba2d2c356fbba1d1b84532793db58ea4e1f9a
g-priime <57049481+g-priime@users.noreply.github.com>
2022-02-12 14:47:24 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const timeLeftDisplay = document.querySelector("#time-left");
 const resultDisplay = document.querySelector("#result");
 const StartPauseButton = document.querySelector("#start-pause-button");
 const squares = document.querySelectorAll(".grid div");
+const logsleft = document.querySelectorAll(".log-left");
+const logsright = document.querySelectorAll(".log-right");
+
 let currentIndex = 76;
 const width = 9;
 
@@ -26,3 +29,60 @@ function moveFrog(e) {
   squares[currentIndex].classList.add("frog");
 }
 document.addEventListener("keyup", moveFrog);
+
+function autoMoveLogs() {
+  logsleft.forEach((logLeft) => moveLogLeft(logLeft));
+  logsright.forEach((logRight) => moveLogRight(logRight));
+}
+
+function moveLogLeft(logLeft) {
+  switch (true) {
+    case logLeft.classList.contains("l1"):
+      logLeft.classList.remove("l1");
+      logLeft.classList.add("l2");
+      break;
+    case logLeft.classList.contains("l2"):
+      logLeft.classList.remove("l2");
+      logLeft.classList.add("l3");
+      break;
+    case logLeft.classList.contains("l3"):
+      logLeft.classList.remove("l3");
+      logLeft.classList.add("l4");
+      break;
+    case logLeft.classList.contains("l4"):
+      logLeft.classList.remove("l4");
+      logLeft.classList.add("l5");
+      break;
+    case logLeft.classList.contains("l5"):
+      logLeft.classList.remove("l5");
+      logLeft.classList.add("l1");
+      break;
+  }
+}
+
+function moveLogRight(logRight) {
+  switch (true) {
+    case logRight.classList.contains("l1"):
+      logRight.classList.remove("l1");
+      logRight.classList.add("l5");
+      break;
+    case logRight.classList.contains("l2"):
+      logRight.classList.remove("l2");
+      logRight.classList.add("l1");
+      break;
+    case logRight.classList.contains("l3"):
+      logRight.classList.remove("l3");
+      logRight.classList.add("l2");
+      break;
+    case logRight.classList.contains("l4"):
+      logRight.classList.remove("l4");
+      logRight.classList.add("l3");
+      break;
+    case logRight.classList.contains("l5"):
+      logRight.classList.remove("l5");
+      logRight.classList.add("l4");
+      break;
+  }
+}
+
+setInterval(autoMoveLogs, 1000);
